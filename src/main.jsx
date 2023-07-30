99606a6e34c68739562ba86133b3c9fe1fd33332
Vasharaladeva <51453264+Vasharaladeva@users.noreply.github.com>
2023-07-29 23:35:38 -0400

    modifique el archivo gitignore
    archivos toma

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
-import NuevoCliente, {action as nuevoClienteAction } from './pages/NuevoCliente'
+import QrScanner, {action as nuevoClienteAction } from './pages/QrScanner'
 import Index, { loader as clientesLoader } from './pages/Index'
 import EditarCliente, { loader as editarClienteLoader, action as editarClienteAction } from './pages/EditarCliente'
 import ErrorPage from './components/ErrorPage'
@@ -25,8 +25,8 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />
       },
       {
-        path: '/clientes/nuevo',
-        element: <NuevoCliente />,
+        path: '/scannerQr',
+        element: <QrScanner />,
         action: nuevoClienteAction,
         errorElement: <ErrorPage />
       },
